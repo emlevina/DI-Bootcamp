62dcdc6b37b8fd535fc5cd1131e9917fd902271f
Ekaterina <katya.leemi@gmail.com>
2023-01-23 10:36:34 +0200

    Week2 Day2 Exercises XP

---
+++ Week2/Day2/ExerciseXP/exercise1.js
@@ -0,0 +1,17 @@
+// Create 2 variables, x and y. Each of them should have a different numeric value.
+// Write an if/else statement that checks which number is bigger.
+// Example :
+
+let x = 5;
+let y = 2;
+
+// You should display:
+// x is the biggest number
+
+if(x > y){
+    console.log('x is the biggest number')
+} else if (y > x) {
+    console.log('y is the biggest number')
+} else {
+    console.log('x and y are equal')
+}
---
+++ Week2/Day2/ExerciseXP/exercise3.js
@@ -0,0 +1,12 @@
+// Prompt the user for a number and save it to a variable.
+// Check whether the variable is even or odd.
+// If it is even, display: “x is an even number”. Where x is the actual number the user chose.
+// If it is odd, display: “x is an odd number”. Where x is the actual number the user chose.
+
+let num = prompt('Please, insert a number')
+
+if(Number(num) % 2 === 0){
+    alert(`${num} is an even number`)
+} else {
+    alert(`${num} is an odd number`)
+}
