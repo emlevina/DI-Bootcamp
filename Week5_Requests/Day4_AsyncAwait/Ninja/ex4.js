f3829aa667ee55f5a768aae07cc6df4a3678d21e
Ekaterina <katya.leemi@gmail.com>
2023-02-15 15:47:56 +0200

    W5d4 Ninja

---
+++ Week5_Requests/Day4_AsyncAwait/Ninja/ex4.js
@@ -0,0 +1,36 @@
+let resolveAfter2Seconds = function () {
+    console.log("starting slow promise");
+    return new Promise(resolve => {
+        setTimeout(function () {
+            resolve("slow");
+            console.log("slow promise is done");
+        }, 2000);
+    });
+};
+
+let resolveAfter1Second = function () {
+    console.log("starting fast promise");
+    return new Promise(resolve => {
+        setTimeout(function () {
+            resolve("fast");
+            console.log("fast promise is done");
+        }, 1000);
+    });
+};
+
+// This function does not handle errors. See warning below!
+let parallelPromise = function () {
+    console.log('==PARALLEL with Promise.then==');
+    resolveAfter2Seconds().then((message) => console.log(message));
+    resolveAfter1Second().then((message) => console.log(message));
+}
+
+setTimeout(parallelPromise, 4000)
+
+// ==PARALLEL with Promise.then==
+// starting slow
+// staring fast
+// fast is done
+// fast
+// slow is done
+// slow
