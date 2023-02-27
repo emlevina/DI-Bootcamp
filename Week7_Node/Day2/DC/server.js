8d559c0040da208997074f7110a94e0910d6b938
Ekaterina <katya.leemi@gmail.com>
2023-02-27 15:47:49 +0200

    W7d2 DC

---
+++ Week7_Node/Day2/DC/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+})
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.use('/form', express.static(__dirname + '/public'))
+// The route /pic : displays an HTML file with a picture of your choice.
+// The route /form : displays an HTML file.
+app.get('/pic', (req, res)=>{
+    res.send(`<img width='100px' src='https://images.unsplash.com/photo-1595433707802-6b2626ef1c91?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80'>`)
+})
+
+app.get('/aboutMe/:hobby', (req, res) => {
+    const hobby = req.params.hobby
+    if (!isNaN(hobby) || !hobby) {
+        // If the parameter is not a string (ie. numbers or else), set the status to 404.
+        res.status(404)
+    } else {
+        // The route /aboutMe/:hobby: displays the name of one hobby (ie. the value of the route parameter).
+        res.json({ hobby })
+    }
+})
+// You should get the data and display it on the browser at the route /formData.
+// For example, “john@gmail.com sent you a message “Love your new website”.
+app.post('/formData', (req, res)=>{
+    console.log(req.body)
+    res.send(`<p>${req.body.email} sent you a message "${req.body.message}"</p>`)
+})
+
+// Requirements:
+// The HTML file must be in the public folder.
+// The HTML file must contain a form to contact you.
+// The form must contain the inputs email and message. (add some HTML form validations)
+// Output:
+// You should get the data and display it on the browser at the route /formData.
+// For example, “john@gmail.com sent you a message “Love your new website”.
--- Week7_Node/Day2/Inclass/ex1/server.js
+++ Week7_Node/Day2/Inclass/ex1/server.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const { products } = require('./modules/products');
-// const cors = require('cors')
-
 
 const app = express();
-// app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use('/', express.static(__dirname + '/public'))
